13702e38d0455849154bc784d17925317487d7b5
Tiansas <sassweet@outlook.com>
2018-07-25 12:41:56 -0500

    Update imu.c

--- imu.c
+++ imu.c
@@ -43,7 +43,7 @@ float pitch,roll,yaw,temp;
 float Ra_g[3][3];
 float preRa_g[3][3];
 /////////////////////////  ITG3205
-//≥ı ºªØITG3205  ITG3205 Initialize
+//ÂàùÂßãÂåñITG3205  ITG3205 Initialize
 void Init_ITG3205(void)
 {	
   Write_single_reg(ITG3205_Addr,ITG3205_PWR_M,0x00);  
@@ -95,7 +95,7 @@ void READ_ITG3205_XYZT(void)
 //////////////////////////// end ITG3205
 
 /////////////////////////  ADXL345
-//≥ı ºªØADXL345  ADXL345 initialize
+//ÂàùÂßãÂåñADXL345  ADXL345 initialize
 void Init_ADXL345(void)
 {
   Write_single_reg(ADXL345_Addr,ADXL345_DATA_FORMAT,0x0B);  //setting the range, here it is set at +-16g with 0x0B
@@ -145,7 +145,7 @@ void ADXL345_Read_XYZt(void)
 
 //	conver_y=ADXL345_Y*4;  //float to int
 	//conver_z=ADXL345_Z*4;
-  //ªÒ»°ADXL345 angle  ADXL345 angle
+  //Ëé∑ÂèñADXL345 angle  ADXL345 angle
 	//printf("#%d$%d$%d$",ADXL345_X,ADXL345_Y,ADXL345_Z);
 	
 
@@ -163,7 +163,7 @@ temp = sqrt( pow(conver_x,2)+pow(conver_y,2)+pow(conver_z,2));
 
 /////////////////////////  HMC5883L
 
-//≥ı ºªØHMC5883L  HMC5883L initialize
+//ÂàùÂßãÂåñHMC5883L  HMC5883L initialize
 void Init_HMC5883L(void)
 {
   Write_single_reg(HMC5883L_Addr,HMC5883L_configA,0x14);  
@@ -174,7 +174,7 @@ void HMC5883L_Read_XYZt(void)
 {
   uint8_t  data_l,data_h;
 	
-	//ªÒ»°ITG3205 X÷·  X_axle
+	//Ëé∑ÂèñITG3205 XËΩ¥  X_axle
 	
 	data_l = Read_single_reg(HMC5883L_Addr,HMC5883L_XL);
 	data_h = Read_single_reg(HMC5883L_Addr,HMC5883L_XH);
@@ -211,7 +211,7 @@ void HMC5883L_Read_XYZt(void)
 //////////////////////////// end HMC5883L
 
 /*Initialize the sensor module 
-** imu_ic parameter value:  IMU_ITG3205°¢IMU_ADXL345°¢IMU_HMC5883L°¢IMU_ALL_IC
+** imu_ic parameter value:  IMU_ITG3205„ÄÅIMU_ADXL345„ÄÅIMU_HMC5883L„ÄÅIMU_ALL_IC
 **  IMU_ITG3205 means ITG3205 initialize,IMU_ADXL345 means ADXL345 initialize
 **  IMU_HMC5883L means HMC5883L initialize,IMU_ALL_IC means Three chips are initialized
 */
@@ -448,44 +448,6 @@ void scalar_multiply(float input[3][3] ,float lambda){
 	
 }
 
-void calculate_p_r_y(){
-	float temp;
-	yaw = atan2(Ra_g[1][0],Ra_g[0][0] )*RADIANS_2_DEGREE;
-	roll = atan2(Ra_g[2][1],Ra_g[2][2])*RADIANS_2_DEGREE;
-	temp = sqrt( pow(Ra_g[2][1],2)+pow(Ra_g[2][2],2));
-	pitch = atan2(-1*Ra_g[2][0],temp)*RADIANS_2_DEGREE;
-	//printf("pitch = %f, roll = %f, yaw = %f\r\n",pitch,roll,yaw);
-}
-
-
-//Inv of Roation matrix of Device GPS match to F150
-/*float inv_v1_x=0.1101;
-float inv_v1_y=0.9928;
-float inv_v1_z=-0.0463;
-float inv_v2_x=0.9837;
-float inv_v2_y=-0.1022;
-float inv_v2_z=0.1477;
-float inv_v3_x=-0.01419;
-float inv_v3_y=0.0618;
-float inv_v3_z=0.9880;*/	
-
-//Inv of Roation matrix of Device No Ca match to F150
-/*float inv_v1_x=-0.20839;
-float inv_v1_y=0.977926;
-float inv_v1_z=-0.01527;
-float inv_v2_x=0.967741;
-float inv_v2_y=0.208432;
-float inv_v2_z=0.141544;
-float inv_v3_x=-0.141602;
-float inv_v3_y=-0.014723;
-float inv_v3_z=0.989815;	 */
-/*
-float offset_hx=0.6848;         //offset of magnetometer measurement
-float offset_hy=-5.2363;
-float offset_hz=-1.5532;
-*/
-
-//characteristic of device T in Tian's car
 // rotation matrix from calibration 
 float inv_v1_x=-0.9999;
 float inv_v1_y=0.0129;
@@ -505,12 +467,13 @@ short offset_gz=0;
 float offset_hx=-2.7006;         //offset of magnetometer measurement
 float offset_hy=3.3772;
 float offset_hz=0.2013;
-//add by Tian£∫Estimation process
+//add by TianÔºöEstimation process
 
 uint16_t test_count=0;
 uint16_t N_test=600;      //try three minute first
 float delta_t=0.1;        //sample rate 10Hz
-float Ax_cal[600],Ay_cal[600],Ax_new[600],Ay_m[600],Gz[600],HA_mag[600];
+float Ax_cal[600],Ay_cal[600],Ax_new[600],Ay_m[600],HA_mag[600];
+short Gz[600];
 int num_turn=0,num_stop=0,num_stop_turn=0;
 int total_num;
 int Stop_Turn_po[10][2];
@@ -527,7 +490,7 @@ void Estimation_process()
 	{
 		//get the acceleration after calibration	
 		Ax_cal[test_count]=cal_ax;
-	  Ay_cal[test_count]=cal_ay;
+	        Ay_cal[test_count]=cal_ay;
 		Gz[test_count]=ITG3205_Z;
 		
 		//heading angle estimation using magnetometer data 
@@ -614,7 +577,7 @@ void Estimation_process()
 			}
 			//detection of stops and turns
 			Turn_detection(Ay_m,Gz);
-      Stop_detection(Ax_new);    
+                        Stop_detection(Ax_new);    
 			End_num_ST=num_stop_turn-1;
 			Rank_stop_turn();
 			if (num_stop_turn==0){
@@ -635,8 +598,6 @@ void Estimation_process()
 				int p=0;
 				for (int k=0;k<num_parts;k++)  
 				{  
-					/*float v_1=Stop_Turn_sp[k];
-					float v_2=Stop_Turn_sp[k+1];*/
 					
 					float v_start=Stop_Turn_sp[k];
 					float v_end=Stop_Turn_sp[k+1];
@@ -855,7 +816,7 @@ void Low_pass_filter(float Acc[600]){
 */
 				
 // Turn detection
-void Turn_detection(float A_y[600],float G_z[600]){
+void Turn_detection(float A_y[600],short G_z[600]){
 	float Angle_gyro[600];
 	float temp_angle=0;
   Angle_gyro[0]=-G_z[0]*delta_t;
@@ -1216,17 +1177,11 @@ int find_limit(float X[],int start,int end,float limit)
 	}
 	return num_limit;
 }
-/*
-// get a two dimensional space for row and column
-void	*Get_space(int num_row,int (*tu_po)[2])	
-{
-	tu_po=(int(*)[2])malloc(num_row*2*sizeof(int));
-} 
-*/
 
 
 
 //add by Tian
+//Auto-calibration process
 //uint16_t count = 0;
 /*uint16_t cal_count = 0;
 uint16_t cal_N_max  = 400;
@@ -1240,7 +1195,7 @@ float v1_x,v1_y,v1_z,v2_x,v2_y,v2_z;
 float inv_v1_x,inv_v1_y,inv_v1_z,inv_v2_x,inv_v2_y,inv_v2_z,inv_v3_x,inv_v3_y,inv_v3_z;	
  
 
-    float a_for_cal[400][3];  //store the acceleration used for callibration under the condition that norm_g<=3  »´æ÷±‰¡ø
+    float a_for_cal[400][3];  //store the acceleration used for callibration under the condition that norm_g<=3  ÂÖ®Â±ÄÂèòÈáè
 		float r_for_fitting[400][3];  //store the residual r used for fitting
     float projection_rx_test[400],projection_ry_test[400];
 		float X_for_direction[400];
@@ -1387,7 +1342,7 @@ float inv_v1_x,inv_v1_y,inv_v1_z,inv_v2_x,inv_v2_y,inv_v2_z,inv_v3_x,inv_v3_y,in
 						X_for_direction[i]=r_for_fitting[i][0]*v1_xx+r_for_fitting[i][1]*v1_yy+r_for_fitting[i][2]*v1_zz;
 					}
 					 
-					//∂‘X¥”–°µΩ¥ÛΩ¯––≈≈–Ú
+					//ÂØπX‰ªéÂ∞èÂà∞Â§ßËøõË°åÊéíÂ∫è
           for (int i=0; i<=cal_N_max-2; i++) {
 						for (int j=i+1; j<=cal_N_max-1; j++) {
 							if (X_for_direction[j]<X_for_direction[i]) {  
@@ -1404,7 +1359,7 @@ float inv_v1_x,inv_v1_y,inv_v1_z,inv_v2_x,inv_v2_y,inv_v2_z,inv_v3_x,inv_v3_y,in
 						}
 							
 					}
-					//º∆À„◊Ó¥Û∫Õ◊Ó–°µƒ∂˛ Æ∏ˆ ˝µƒ∫Õ
+					//ËÆ°ÁÆóÊúÄÂ§ßÂíåÊúÄÂ∞èÁöÑ‰∫åÂçÅ‰∏™Êï∞ÁöÑÂíå
 					for(int i=0;i<20;i++){
 						X_neg=X_neg+X_for_direction[i];
 						k=cal_N_max-1-i;
@@ -1462,114 +1417,3 @@ float inv_v1_x,inv_v1_y,inv_v1_z,inv_v2_x,inv_v2_y,inv_v2_z,inv_v3_x,inv_v3_y,in
 			}
 	}*/
 
-
-
-	/*//We now start the process of obtaining attitude_rotation matrix
-		
-		//1. we get the normalized callibrated_a vector
-		
-		float norm_a = sqrt( pow(conver_x,2)+pow(conver_y,2)+pow(conver_z,2));
-		
-		float normalized_a_x = conver_x/norm_a;
-		float normalized_a_y = conver_y/norm_a;
-		float normalized_a_z = conver_z/norm_a;
-
-		//2. we get the normalized projection of the magnetic field vector b on a plane
-		//perpendicular to a
-		
-		float b_x = HMC5883L_X - (HMC5883L_X*normalized_a_x+HMC5883L_Y*normalized_a_y+HMC5883L_Z*normalized_a_z)*normalized_a_x;
-		float b_y = HMC5883L_Y - (HMC5883L_X*normalized_a_x+HMC5883L_Y*normalized_a_y+HMC5883L_Z*normalized_a_z)*normalized_a_y;
-		float b_z = HMC5883L_Z - (HMC5883L_X*normalized_a_x+HMC5883L_Y*normalized_a_y+HMC5883L_Z*normalized_a_z)*normalized_a_z;
-		
-		float norm_b = sqrt( pow(b_x,2)+pow(b_y,2)+pow(b_z,2));
-		
-		float normalized_b_x = b_x/norm_b;
-		float normalized_b_y = b_y/norm_b;
-		float normalized_b_z = b_z/norm_b;
-		
-		//3. we take the cross product of a and b to obtain c
-		float c_x = normalized_a_y*normalized_b_z - normalized_b_y*normalized_a_z;
-		float c_y = normalized_a_x*normalized_b_z - normalized_a_z*normalized_b_x;
-		float c_z = normalized_a_x*normalized_b_y - normalized_a_y*normalized_b_x;
-		
-		
-		
-		R_ref[0][0] = normalized_a_x;
-		R_ref[1][0] = normalized_a_y;
-		R_ref[2][0] = normalized_a_z;
-		R_ref[0][1] = normalized_b_x;
-		R_ref[1][1] = normalized_b_y;
-		R_ref[2][1] = normalized_b_z;
-		R_ref[0][2] = c_x;
-		R_ref[1][2] = c_y;
-		R_ref[2][2] = c_z;
-		
-		//4. we obtain an elementary matrix
-		R_elem[0][0] = 1;
-		R_elem[1][0] = ITG3205_Z*delta_t;
-		R_elem[2][0] = -ITG3205_Y*delta_t;
-		R_elem[0][1] = -ITG3205_Z*delta_t;
-		R_elem[1][1] = 1;
-		R_elem[2][1] = ITG3205_X*delta_t;
-		R_elem[0][2] = ITG3205_Y*delta_t;
-		R_elem[1][2] = -ITG3205_X*delta_t;
-		R_elem[2][2] = 1;
-		
-		// 5. We initialize R_t matrix only once for the first time
-		if (R_t_initial == 0) {
-			R_t_initial = 1;
-			R_t[0][0] = 0;
-			R_t[1][0] = 0;
-			R_t[2][0] = 0;
-			R_t[0][1] = 0;
-			R_t[1][1] = 0;
-			R_t[2][1] = 0;
-			R_t[0][2] = 0;
-			R_t[1][2] = 0;
-			R_t[2][2] = 0;
-		
-		}
-		
-		
-		//6. We now get the callibrated acceleration
-		
-		
-		float temp_conver_x = conver_x - (R_t[0][0]*0 + R_t[0][1]*0 + R_t[0][2]*g);
-		float temp_conver_y = conver_y - (R_t[1][0]*0 + R_t[1][1]*0 + R_t[1][2]*g);
-		float temp_conver_z = conver_z - (R_t[2][0]*0 + R_t[2][1]*0 + R_t[2][2]*g);
-		
-		callibrated_a_x = inv_rot_matrix[0][0]*temp_conver_x + inv_rot_matrix[1][0]*temp_conver_y + inv_rot_matrix[2][0]*temp_conver_z;
-		callibrated_a_y = inv_rot_matrix[1][0]*temp_conver_x + inv_rot_matrix[1][1]*temp_conver_y + inv_rot_matrix[1][2]*temp_conver_z;
-		callibrated_a_z = inv_rot_matrix[2][0]*temp_conver_x + inv_rot_matrix[2][1]*temp_conver_y + inv_rot_matrix[2][2]*temp_conver_z;
-		
-		
-		
-		//7. We now make the update step on R_t with new values to use in the next iteration
-		float rt1_1x = lambda*R_elem[0][0]*R_t[0][0] + lambda*R_elem[0][1]*R_t[1][0] + lambda*R_elem[0][2]*R_t[2][0];
-		float rt1_1y = lambda*R_elem[1][0]*R_t[0][0] + lambda*R_elem[1][1]*R_t[1][0] + lambda*R_elem[1][2]*R_t[2][0];
-		float rt1_1z = lambda*R_elem[2][0]*R_t[0][0] + lambda*R_elem[2][1]*R_t[1][0] + lambda*R_elem[2][2]*R_t[2][0];
-			
-		float rt1_2x = lambda*R_elem[0][0]*R_t[0][1] + lambda*R_elem[0][1]*R_t[1][1] + lambda*R_elem[0][2]*R_t[2][1];
-		float rt1_2y = lambda*R_elem[1][0]*R_t[0][1] + lambda*R_elem[1][1]*R_t[1][1] + lambda*R_elem[1][2]*R_t[2][1];
-		float rt1_2z = lambda*R_elem[2][0]*R_t[0][1] + lambda*R_elem[2][1]*R_t[1][1] + lambda*R_elem[2][2]*R_t[2][1];
-			
-		float rt1_3x = lambda*R_elem[0][0]*R_t[0][2] + lambda*R_elem[0][1]*R_t[1][2] + lambda*R_elem[0][2]*R_t[2][2];
-		float rt1_3y = lambda*R_elem[1][0]*R_t[0][2] + lambda*R_elem[1][1]*R_t[1][2] + lambda*R_elem[1][2]*R_t[2][2];
-		float rt1_3z = lambda*R_elem[2][0]*R_t[0][2] + lambda*R_elem[2][1]*R_t[1][2] + lambda*R_elem[2][2]*R_t[2][2];
-		
-		R_t[0][0] = rt1_1x + (1-lambda)*R_ref[0][0];
-		R_t[1][0] = rt1_1y + (1-lambda)*R_ref[1][0];
-		R_t[2][0] = rt1_1z + (1-lambda)*R_ref[2][0];
-		
-		R_t[0][1] = rt1_2x + (1-lambda)*R_ref[0][1];
-		R_t[1][1] = rt1_2y + (1-lambda)*R_ref[1][1];
-		R_t[2][1] = rt1_2z + (1-lambda)*R_ref[2][1];
-		
-		R_t[0][2] = rt1_3x + (1-lambda)*R_ref[0][2];
-		R_t[1][2] = rt1_3y + (1-lambda)*R_ref[1][2];
-		R_t[2][2] = rt1_3z + (1-lambda)*R_ref[2][2];
-		
-	}
-	
-	//printf("R: %f, %f and %f.\n", r_ca[0], r_ca[1], r_ca[2]);
-} */ 
